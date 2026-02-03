a56f8b89b185f62580b5734d33b5773459502866
vboxuser <ronielsonoliveira@live.com>
2026-02-03 17:25:50 -0300

    layout changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Routes from "./routes/index";
-import Header from "./components/Header";
 import GlobalStyles from "./styles/GlobalStyles";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
